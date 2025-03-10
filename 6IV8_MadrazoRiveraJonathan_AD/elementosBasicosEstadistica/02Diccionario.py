e355c44fe60d2809a2eacbe3ffc34b5261aa535d
Jonny0714 <madrazo.rivera.jonathan@gmail.com>
2025-03-10 15:44:23 -0600

    Cambio por aprobados

--- 6IV8_MadrazoRiveraJonathan_AD/elementosBasicosEstadistica/02Diccionario.py
+++ 6IV8_MadrazoRiveraJonathan_AD/elementosBasicosEstadistica/02Diccionario.py
@@ -4,5 +4,12 @@ def estadistica_notas(notas):
     notas= pd.Series(notas) 
     estadisticas = pd.Series([notas.min(),notas.max(),notas.mean(),notas.std()],index=['Minimo','Maximo','Media','Desviacion estandar'])
     return estadisticas
-notas = {'Juan':9,'Juanita':7,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,'Sandra':9.8,'Rosario':9}
+notas = {'Juan':5.9,'Juanita':5,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,'Sandra':9.8,'Rosario':9}
+
+
+def aprobados(notas):
+    notas= pd.Series(notas)
+    aprobados = notas[notas>=5].sort_values(ascending=False)
+    return aprobados
+print(aprobados(notas))
 print(estadistica_notas(notas))  
